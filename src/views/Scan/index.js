094f2434a6ef452eaa0dd001c829f01e7d99508e
Temoso Masilo <msltem001@myuct.ac.za>
2021-08-11 15:12:48 +0200

    Connected Scan page frontend to backend & updated api to save corrected userId

--- src/views/Scan/index.js
+++ src/views/Scan/index.js
@@ -1,11 +1,116 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppLayout from '../../layout/AppLayout';
+import axios from "axios";
 import { Row } from "reactstrap";
 import { Colxx, Separator } from "../../components/common/CustomBootstrap";
 import './scan.scss';
 
 const Scan = (props) => {
-  const [activeScan, setActiveScan] = useState("single");
+    const [activeScan, setActiveScan2] = useState("single");
+
+    //Dummy user details retrieved
+    let retrievedUserDetails = {
+        _id: "ahsusdfrthjeni1246463",
+        name: "Coca Cola",
+        role: "Manufacturer",
+        img: "https://mpng.subpng.com/20190527/huu/kisspng-the-coca-cola-company-logo-thredbo-australias-best-winter-summer-alpine-5ceba0b9d8f945.6662259515589459778887.jpg"
+      }
+
+    const [currentUser] = useState(retrievedUserDetails);
+    const [isLoading, setIsLoading] = useState(false);
+    const [roleOptions, setroleOptions] = useState([]);
+    const [data, setData] = useState({
+        "manufacturer":{
+            id: retrievedUserDetails._id,
+            name: retrievedUserDetails.name
+        },
+        "isBatch": false,
+        "sizeUnit":"ml",
+        "bottleType": "PET",
+        "isNewScan":retrievedUserDetails.role === "Manufacturer" ? "true" : "false",
+        "userId": retrievedUserDetails._id
+    });
+
+   
+    
+    useEffect(() => {
+         //Declare the available status to each user role in arrays
+        const ManufacturerOptions = ["Manufactured", "Outgoing"];
+        const RetailerOptions = ["Delivered", "Purchased"];
+        const ConsumerOptions = ["Deposited"];
+        const WastePickerOptions = ["Deposited"];
+        const RecyclingDepotOptions = ["Recycled"]
+
+        if(currentUser.role === "Manufacturer"){
+            setroleOptions(ManufacturerOptions);
+            setData({...data,"bottleStatus":ManufacturerOptions[0]});
+        }
+        else if(currentUser.role === "Retailer"){
+            setroleOptions(RetailerOptions);
+            setData({...data,"bottleStatus":RetailerOptions[0]});
+        }
+        else if(currentUser.role === "Consumer"){
+            setroleOptions(ConsumerOptions);
+            setData({...data,"bottleStatus":ConsumerOptions[0]});
+        }
+        else if(currentUser.role === "Waste Picker"){
+            setroleOptions(WastePickerOptions);
+            setData({...data,"bottleStatus":WastePickerOptions[0]});
+        }
+        else if(currentUser.role === "Recycling Depot"){
+            setroleOptions(RecyclingDepotOptions);
+            setData({...data,"bottleStatus":RecyclingDepotOptions[0]});
+        }
+       
+    }, [currentUser])
+
+    //Add the user's input into the data sent to the backend
+    const setDataValue = e => {
+        setData({...data, [e.target.name]:e.target.value});
+    }
+
+    const setActiveScan = type => {
+        setData({...data, "isBatch": type === "single" ? false : true})
+        setActiveScan2(type);
+    }
+
+    //Function to begin the scan after form is submitted
+    const BeginScan = (e) => {
+        setIsLoading(true);
+        e.preventDefault();
+
+        let correctEndpoint = "addbottle";
+        if(data.isNewScan === "true")
+        {
+            correctEndpoint = "addbottle"
+        }
+        else{
+            correctEndpoint = "updatebottle"
+        }
+        var config = {
+        method: 'post',
+        url: 'http://localhost:5000/api/'+correctEndpoint,
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+            alert(response.data.message);
+            console.log(response.data);
+            setIsLoading(false);
+        })
+        .catch(function (error) {
+            let errorList = error.response.data.errors;
+            for(let i = 0; i < errorList.length; i++){
+                alert(errorList[i])
+            }
+            setIsLoading(false);
+        });
+
+    }
     return(
         <AppLayout>
             <Row>
@@ -45,58 +150,89 @@ const Scan = (props) => {
                     <div className="card mb-4">
                         <div className="card-body">
                             <h5 className="mb-4">Product Details</h5>
-                            <form>
+                            <form onSubmit={(e) => BeginScan(e)}>
+                                
+                                {
+                                    //Only a manufacturer should see this option
+                                    currentUser.role === "Manufacturer" &&
+                                    <div className="form-group row">
+                                        <label className="col-sm-2 col-form-label">Scan Action</label>
+                                        <div className="col-sm-10">
+                                        <select required  name="isNewScan" onChange={(e) => setDataValue(e)} className="form-control">
+                                            <option value={true}>New Scan</option>
+                                            <option value={false}>Update Previous Scan</option>                                           
+                                        </select>
+                                        </div>
+                                    </div>
+                                }
+
+                                {
+                                data.isNewScan === "true" &&
                                 <div className="form-group row">
-                                    <label className="col-sm-2 col-form-label">Product Title</label>
+                                    <label className="col-sm-2 col-form-label">Bottle Title</label>
                                     <div className="col-sm-10">
-                                        <input type="text" className="form-control" />
+                                        <input required name="title" onChange={(e) => setDataValue(e)} type="text" className="form-control" />
                                     </div>
                                 </div>
+                                }
+
+
+
                                 <div className="form-group row">
                                     <label className="col-sm-2 col-form-label">Status</label>
                                     <div className="col-sm-10">
-                                        <select className="form-control">
-                                            <option>Manufactured</option>
-                                            <option>Outgoing</option>
-                                            <option>Delivered</option>
-                                            <option>Purchased</option>
-                                            <option>Deposited</option>
-                                            <option>Recycled</option>
+                                        <select required  className="form-control" name="bottleStatus" onChange={(e) => setDataValue(e)}>
+                                           {
+                                               roleOptions.map((item, i) => {
+                                                   return <option key={i}>{item}</option>
+                                               })
+                                           }
                                         </select>
                                     </div>
                                 </div>
+                                
+                                {
+                                activeScan === "single" ?
                                 <div className="form-group row">
-                                    <label className="col-sm-2 col-form-label">Product Description</label>
+                                    <label className="col-sm-2 col-form-label">Bottle QR</label>
                                     <div className="col-sm-10">
-                                        <textarea style={{height:173}} className="form-control" ></textarea>
+                                        <input required name="bottleQr" onChange={(e) => setDataValue(e)} type="text" className="form-control" />
                                     </div>
                                 </div>
-                                <fieldset className="form-group">
-                                    <div className="row">
-                                        <label className="col-form-label col-sm-2 pt-0">Product Type</label>
+                                :
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label">Batch QR</label>
+                                    <div className="col-sm-10">
+                                        <input required name="batchQr" onChange={(e) => setDataValue(e)} type="text" className="form-control" />
+                                    </div>
+                                </div>
+                                }
+                                {
+                                   data.isNewScan === "true" &&
+                                    <>
+                                    <div className="form-group row">
+                                        <label className="col-sm-2 col-form-label">Bottle Size</label>
                                         <div className="col-sm-10">
-                                            <div className="form-check">
-                                                <input className="form-check-input" type="radio" name="gridRadios"
-                                                    id="gridRadios1" value="option1" checked />
-                                                <label className="form-check-label" for="gridRadios1">
-                                                    Incoming
-                                                </label>
-                                            </div>
-                                            <div className="form-check">
-                                                <input className="form-check-input" type="radio" name="gridRadios"
-                                                    id="gridRadios2" value="option2" />
-                                                <label className="form-check-label" for="gridRadios2">
-                                                    Outgoing
-                                                </label>
-                                            </div>
-                                        
+                                            <input type="number" required name="bottleSize" onChange={(e) => setDataValue(e)}  className="form-control" />
                                         </div>
                                     </div>
-                                </fieldset>
+                                    <div className="form-group row">
+                                        <label className="col-sm-2 col-form-label">Bottle Size Unit</label>
+                                        <div className="col-sm-10">
+                                        <select required  className="form-control" name="sizeUnit" onChange={(e) => setDataValue(e)}>
+                                                <option value="ml">ML (Millilitre)</option>
+                                                <option value="l">L (Litre)</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                               
+                                    </>
+                                }
+                                
                             
                                 <div className="form-group row mb-0">
                                     <div className="col-sm-10">
-                                        <button type="submit" className="btn btn-primary mb-0"><i className="iconsminds-qr-code"></i> Begin Scan</button>
+                                        <button  disabled={isLoading} className="btn btn-primary mb-0"><i className="iconsminds-qr-code"></i> { isLoading ? "Please wait.." : "Submit"} </button>
                                     </div>
                                 </div>
                             </form>
