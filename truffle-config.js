41cfca12caa6894ab2e7ce727b79620e2bc5eab6
Daniel Aaron <danielaaron7@gmail.com>
2021-12-06 08:44:30 +0200

    Updated Blockchain Linking

--- truffle-config.js
+++ truffle-config.js
@@ -18,12 +18,13 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 //const infuraKey = "80a5bac3bc5640da93695aa4a1bfe90c";
 //const fs = require('fs');
-const mnemonic = "accident stone junk culture debris fat shine skate slight diary recall pulp"
 
-
+//Add your own personal Mnemonic from MetaMask below (This one is only accessible to us)
+const mnemonic =
+  "accident stone junk culture debris fat shine skate slight diary recall pulp";
 
 module.exports = {
   /**
@@ -43,10 +44,10 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
     // Another network with more advanced options...
     // advanced: {
@@ -59,17 +60,23 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+
+    //The link below must be the same as the one on the ropsten site
     ropsten: {
-    provider: () => 
-    new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/80a5bac3bc5640da93695aa4a1bfe90c`),
-    network_id: 3,       // Ropsten's id
-    //gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-   // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-   // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-     },
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/80a5bac3bc5640da93695aa4a1bfe90c`
+        ),
+      network_id: 3, // Ropsten's id
+
+      //gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
-   //  private: {
+    //  private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
@@ -93,7 +100,7 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
@@ -103,17 +110,17 @@ module.exports = {
   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
   // make a backup of your artifacts to a safe location before enabling this feature.
   //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
